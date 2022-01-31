91dde77d8da3cc5cd9bc6474bb3284eaf4816262
Oluwafunmilayo Dada <oluwafunmilayodada2018@gmail.com>
2022-01-31 03:21:40 +0100

    Updates

---
+++ precache-manifest.8dbdd82cb36bfa721d2b9d01d4a7632d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5431602189e24e38aa0e2422298aa3f",
+    "url": "/Ariiieee/eCommerce-Ui.git/index.html"
+  },
+  {
+    "revision": "92191c9ef535bb71417a",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/js/2.00f43e0a.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/js/2.00f43e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93c2d92e957a7eac6826",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/js/main.42d93c91.chunk.js"
+  },
+  {
+    "revision": "9c9718b2b2bdab40f368",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/js/runtime-main.9a5871b8.js"
+  },
+  {
+    "revision": "1efcba258328c4cdaa30570d74eff1fd",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/media/Fashion.1efcba25.png"
+  },
+  {
+    "revision": "827e34215211cb9cc9db183da2578664",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/media/Refridgerator.827e3421.png"
+  },
+  {
+    "revision": "9b1a18d451cdadece98433969de5b2c5",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/media/electronics.9b1a18d4.png"
+  },
+  {
+    "revision": "b72846d9cad96b8f7a5453dbd3302bb0",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/media/health-beauty.b72846d9.png"
+  },
+  {
+    "revision": "35639ad1a7b8eb827a198b6005d89c2c",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/media/hero-image.35639ad1.png"
+  },
+  {
+    "revision": "1abc644746a55e7ae9ebab68740e8239",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/media/iphone11.1abc6447.png"
+  },
+  {
+    "revision": "39b6b7ef9d235405f4841cfe99cf6e07",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/media/phone-accessories.39b6b7ef.png"
+  },
+  {
+    "revision": "e40e0094ff7ee3c7c34b20a0ade27a4e",
+    "url": "/Ariiieee/eCommerce-Ui.git/static/media/womanezone.e40e0094.png"
+  }
+]);
